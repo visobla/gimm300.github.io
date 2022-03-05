ef20e3797b20ea47e737ada7fa4522dfe07dc6b2
Betapig1 <rynahall607@gmail.com>
2022-03-05 13:13:01 -0700

    Seperation

---
+++ Poker/Socket.js
@@ -0,0 +1,178 @@
+var code;
+var name;
+//Connects to server
+var socket = io("https://pokergimm.herokuapp.com/");
+//Creates listener for when a new player joins, the server will send this out
+socket.on("newPlayer", () => {
+  var send = { code: code };
+  console.log(send);
+  //Client requests all info from the player and processes
+  socket.emit("getPlayerInfo", send, (response) => {
+    var div = document.getElementById("names");
+
+    div.innerHTML = "";
+    console.log(response);
+    var container = document.createElement("div");
+    var board = document.getElementById("board");
+    board.innerText = JSON.stringify(response[0].board);
+
+    for (var x = 0; x < response.length; x++) {
+      var nameFile = document.createElement("h2");
+      var pot = document.getElementById("potAmount");
+      console.log("POT", response[x].pot);
+      if (response[x].pot == undefined && response[x].host) {
+        pot.innerText = "Pot: " + "0";
+      } else if (response[x].pot != undefined && response[x].host) {
+        pot.innerText = "Pot: " + response[x].pot;
+      }
+      console.log("INITIAL", document.getElementById("bet"));
+      nameFile.innerText = response[x].name;
+      if (document.getElementById("bet") == null) {
+        var bet = document.createElement("button");
+        bet.id = "bet";
+      }
+      if (document.getElementById("fold") == null) {
+        var fold = document.createElement("button");
+        fold.id = "fold";
+      }
+
+      var betAmount = document.createElement("input");
+
+      betAmount.id = "amount";
+      //Processes bet and updates the server, refreshes server which updates all the clients, and then asks the server to update the turn order
+      bet.onclick = function bet() {
+        console.log("CLICK");
+        var localBet = document.getElementById("amount");
+        var send = { bet: localBet.value, code: code };
+
+        socket.emit("bet", send, (response) => {
+          console.log(response);
+          socket.emit("refresh", send, (res) => {
+            socket.emit("nextTurn", send, (res) => {});
+          });
+        });
+      };
+      bet.innerText = "Bet";
+      fold.innerText = "Fold";
+
+      console.log(response[x].name.toString(), name);
+      if (response[x].name.toString() == name) {
+        nameFile.appendChild(bet);
+        nameFile.appendChild(betAmount);
+        nameFile.appendChild(fold);
+      }
+
+      var money = document.createElement("h2");
+      money.innerText = response[x].money;
+      if (response[x].cards != undefined) {
+        var cards = document.createElement("p");
+        cards.innerText = JSON.stringify(response[x].cards);
+        container.appendChild(cards);
+      }
+      container.appendChild(nameFile);
+      container.appendChild(money);
+    }
+
+    div.appendChild(container);
+    var data = response;
+    console.log("New", data);
+    var bet = document.getElementById("bet");
+    var fold = document.getElementById("fold");
+    bet.disabled = true;
+    fold.disabled = true;
+    console.log(fold);
+    console.log(bet);
+    console.log("gothere");
+    //Each client determines which players turn it is based off the server info
+    for (var y = 0; y < data.length; y++) {
+      console.log(data[y].turn);
+      if (name == data[y].name && data[y].turn) {
+        console.log("ACTIVATE");
+        bet.disabled = false;
+        fold.disabled = false;
+      }
+    }
+  });
+});
+//Listens for turn change, and updates personal client
+socket.on("changeTurn", (data) => {
+  console.log("New", data);
+  var bet = document.getElementById("bet");
+  var fold = document.getElementById("fold");
+  bet.disabled = true;
+  fold.disabled = true;
+  console.log(fold);
+  console.log(bet);
+  console.log("gothere");
+  for (var y = 0; y < data.length; y++) {
+    console.log(data[y].turn);
+    if (name == data[y].name && data[y].turn) {
+      console.log("ACTIVATE");
+      bet.disabled = false;
+      fold.disabled = false;
+    }
+  }
+});
+//Creates deck, test function, not currently used
+function go() {
+  console.log("here");
+  socket.emit("makeDeck", (res) => {
+    console.log(res);
+  });
+  console.log("got");
+}
+//Draws card. Test function, not currently used
+function draw() {
+  console.log("here");
+  socket.emit("draw", (res) => {
+    console.log(res);
+  });
+  console.log("got");
+}
+//Tells server to create lobby and deck. Attatches deck to host socket
+function create() {
+  name = document.getElementById("name").value;
+  var send = { name: document.getElementById("name").value };
+  console.log("here");
+  socket.emit("createGame", send, (res) => {
+    console.log(res);
+    code = res;
+  });
+  console.log("got");
+}
+//Gives code to server for it to check if valid, if it is it joins the game
+function join() {
+  name = document.getElementById("name").value;
+  console.log("here");
+  code = document.getElementById("gameCode").value;
+  var send = {
+    code: document.getElementById("gameCode").value,
+    name: document.getElementById("name").value,
+  };
+  console.log(send);
+  socket.emit("joinLobby", send, (res) => {
+    console.log(send);
+    if (res.error) {
+      alert("Game not found");
+      return;
+    }
+    socket.emit("getPlayerInfo", send, (response) => {
+      console.log(response);
+    });
+  });
+  console.log("got");
+}
+//Tells the server to start the game, draws each players cards, as well as the middle card. Turn card processing to be done on front end
+function startGame() {
+  var send = { code: code };
+  socket.emit("hands", send, (response) => {
+    console.log(response);
+
+    socket.emit("refresh", send, (res) => {});
+  });
+}
+//Ends the game, right now is button, should be done automatically on last turn
+function endGame() {
+  var send = { code: code };
+  socket.emit("end", send);
+}
--- Poker/demo.js
+++ Poker/demo.js
@@ -1,22 +1,21 @@
-var Deck = require("./Deck")
+var Deck = require("./Deck");
 
-
-var _ = require('lodash');  //require "lodash"
-const express = require('express');
+var _ = require("lodash"); //require "lodash"
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-let gameList=[]
-let gameData={};
+let gameList = [];
+let gameData = {};
 
 const socket = require("socket.io")(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
-    }
-  });
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Test");
 });
 
@@ -25,263 +24,248 @@ function makeId(length) {
   var result = "";
   var characters = "0123456789";
   for (var i = 0; i < length; i++) {
-      result += characters[Math.floor(Math.random() * characters.length)];
+    result += characters[Math.floor(Math.random() * characters.length)];
   }
-  return gameList.includes(result)?makeId(length):result;
-};
+  return gameList.includes(result) ? makeId(length) : result;
+}
 
-function mainBoard(deck){
-  var hands = []
-  for(var x=0;x<5;x++){
-    hands.push(deck.draw())
+function mainBoard(deck) {
+  var hands = [];
+  for (var x = 0; x < 5; x++) {
+    hands.push(deck.draw());
   }
   return hands;
 }
 
-function deal(players,deck){
-  var hands = []
-  for(var x=0;x<players;x++){
-    hands[x]=[deck.draw(),deck.draw()]
+function deal(players, deck) {
+  var hands = [];
+  for (var x = 0; x < players; x++) {
+    hands[x] = [deck.draw(), deck.draw()];
   }
   return hands;
 }
 
-socket.on('connection', (socket) => {
-  console.log('a user connected');
+socket.on("connection", (socket) => {
+  console.log("a user connected");
 
-  socket.on("refresh",(data,callback)=>{
-    socket.emit("newPlayer")
-    for(var x = 0; x<gameData[data.code].length;x++){
-      gameData[data.code][x].emit("newPlayer")
+  socket.on("refresh", (data, callback) => {
+    socket.emit("newPlayer");
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      gameData[data.code][x].emit("newPlayer");
     }
-    callback("Refreshed")
-  })
+    callback("Refreshed");
+  });
 
-  socket.on("bet",(data, callback)=>{
+  socket.on("bet", (data, callback) => {
     //console.log(data)
-    
-        for(var x = 0; x<gameData[data.code].length;x++){
-          if(gameData[data.code][x].user==socket.user){
-            console.log("match",data.bet)
-            gameData[data.code][x].money-=data.bet;
-           // console.log(gameData[data.code][x].money)
-          }
-      if(gameData[data.code][x].host){
-        if(gameData[data.code][x].pot==undefined){
-          gameData[data.code][x].pot=0;
-        gameData[data.code][x].pot+=parseInt(data.bet)}
-      
-      else{
-        //console.log("not undefiend", data)
-        gameData[data.code][x].pot+=parseInt(data.bet)
+
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      if (gameData[data.code][x].user == socket.user) {
+        console.log("match", data.bet);
+        gameData[data.code][x].money -= data.bet;
+        // console.log(gameData[data.code][x].money)
+      }
+      if (gameData[data.code][x].host) {
+        if (gameData[data.code][x].pot == undefined) {
+          gameData[data.code][x].pot = 0;
+          gameData[data.code][x].pot += parseInt(data.bet);
+        } else {
+          //console.log("not undefiend", data)
+          gameData[data.code][x].pot += parseInt(data.bet);
+        }
       }
     }
+    socket.emit("newPlayer");
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      gameData[data.code][x].emit("newPlayer");
     }
-    socket.emit("newPlayer")
-    for(var x = 0; x<gameData[data.code].length;x++){
-      gameData[data.code][x].emit("newPlayer")
-    }
-    callback("Bet Added")
-  })
+    callback("Bet Added");
+  });
 
-  socket.on("createGame",(data,callback)=>{
-    socket.user=data.name;
-   // console.log(data.name)
+  socket.on("createGame", (data, callback) => {
+    socket.user = data.name;
+    // console.log(data.name)
     socket.money = 1500;
     socket.host = true;
-    var temp = new Deck;
+    var temp = new Deck();
     temp.shuffleDeck();
     socket.deck = temp;
-    socket.turn=true;
+    socket.turn = true;
 
     //console.log("req",cards.draw())
-    var code = makeId(5)
-    gameList.push(code)
-   gameData[code]=[]
-    gameData[code].push(socket)
+    var code = makeId(5);
+    gameList.push(code);
+    gameData[code] = [];
+    gameData[code].push(socket);
     //console.log(gameData[code])
-    callback(code)
-    socket.emit("newPlayer")
-})
-socket.on("joinLobby",(data,callback)=>{
-  //console.log("req",cards.draw())
- //console.log("First user",socket.user)
-  if(gameData[data.code]!=null){
-    socket.user=data.name;
-    socket.money=1500;
-    socket.host=false;
-    socket.turn=false;
+    callback(code);
+    socket.emit("newPlayer");
+  });
+  socket.on("joinLobby", (data, callback) => {
+    //console.log("req",cards.draw())
+    //console.log("First user",socket.user)
+    if (gameList.includes(data.code)) {
+      socket.user = data.name;
+      socket.money = 1500;
+      socket.host = false;
+      socket.turn = false;
 
-    gameData[data.code].push(socket)
-   // console.log("First user again",gameData[data.code])
-    //console.log(gameData[data.code])
-    callback("Joined Game")
-    socket.emit("newPlayer")
-    for(var x = 0; x<gameData[data.code].length;x++){
-      gameData[data.code][x].emit("newPlayer")
-    }
-  }
-  else{
-    callback("Game does not exist")
-  }
-  
-})
-
-socket.on("nextTurn",(data,callback)=>{
-  var sendData=[]
-  var found=false
-  console.log("FINDING NEXT TURN")
-  for(var x = 0; x<gameData[data.code].length;x++){
-    console.log(gameData[data.code])
-    if(gameData[data.code][x].turn&&!found){
-      if(x==gameData[data.code].length-1){
-        found=true
-        gameData[data.code][0].turn=true;
-        gameData[data.code][gameData[data.code].length-1].turn=false;
-        
+      gameData[data.code].push(socket);
+      // console.log("First user again",gameData[data.code])
+      //console.log(gameData[data.code])
+      callback("Joined Game");
+      socket.emit("newPlayer");
+      for (var x = 0; x < gameData[data.code].length; x++) {
+        gameData[data.code][x].emit("newPlayer");
       }
-      else{
-        found=true
-        gameData[data.code][x].turn=false
-        gameData[data.code][x+1].turn=true
-        
+    } else {
+      callback({ error: true });
+    }
+  });
+
+  socket.on("nextTurn", (data, callback) => {
+    var sendData = [];
+    var found = false;
+    console.log("FINDING NEXT TURN");
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      console.log(gameData[data.code]);
+      if (gameData[data.code][x].turn && !found) {
+        if (x == gameData[data.code].length - 1) {
+          found = true;
+          gameData[data.code][0].turn = true;
+          gameData[data.code][gameData[data.code].length - 1].turn = false;
+        } else {
+          found = true;
+          gameData[data.code][x].turn = false;
+          gameData[data.code][x + 1].turn = true;
+        }
       }
+      //console.log(gameData[data.code][x])
     }
-    //console.log(gameData[data.code][x])
-  }
-  for(var y = 0; y<gameData[data.code].length;y++){
-    var x = y;
-    var temp = {
-      name:gameData[data.code][x].user,
-      money:gameData[data.code][x].money,
-      host:gameData[data.code][x].host,
-      cards:gameData[data.code][x].cards,
-      pot:gameData[data.code][x].pot,
-      turn:gameData[data.code][x].turn
+    for (var y = 0; y < gameData[data.code].length; y++) {
+      var x = y;
+      var temp = {
+        name: gameData[data.code][x].user,
+        money: gameData[data.code][x].money,
+        host: gameData[data.code][x].host,
+        cards: gameData[data.code][x].cards,
+        pot: gameData[data.code][x].pot,
+        turn: gameData[data.code][x].turn,
+      };
+      console.log(temp);
+      sendData.push(temp);
     }
-    console.log(temp)
-    sendData.push(temp)
- }
-  socket.emit("changeTurn",sendData)
-  for(var x = 0; x<gameData[data.code].length;x++){
-    gameData[data.code][x].emit("changeTurn",sendData)
-  }
-  callback(sendData)
-})
-
-socket.on("getPlayerInfo",(data,callback)=>{
-  //console.log("req",cards.draw())
-
-  var sendData = []
-  //console.log(gameData[data.code])
-  for(var x = 0; x<gameData[data.code].length;x++){
-    //console.log(gameData[data.code][x])
-    var temp = {
-      name:gameData[data.code][x].user,
-      money:gameData[data.code][x].money,
-      host:gameData[data.code][x].host,
-      cards:gameData[data.code][x].cards,
-      pot:gameData[data.code][x].pot,
-      turn:gameData[data.code][x].turn,
-      board:gameData[data.code][x].board
+    socket.emit("changeTurn", sendData);
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      gameData[data.code][x].emit("changeTurn", sendData);
     }
-    sendData.push(temp)
-  }
-  callback(sendData)
-})
+    callback(sendData);
+  });
 
-  socket.on("makeDeck",(callback)=>{
+  socket.on("getPlayerInfo", (data, callback) => {
     //console.log("req",cards.draw())
 
-    callback("Deck Created")
-})
-socket.on("draw",(callback)=>{
-  //console.log("req",cards.draw())
-
-  callback(socket.deck.draw())
-})
-socket.on("hands",(data,callback)=>{
-  //console.log("req",cards.draw())
-  var hands = deal(gameData[data.code].length, socket.deck)
-  var board = mainBoard(socket.deck)
-  var send = []
-
-  for(var x = 0; x<gameData[data.code].length;x++){
-
-    gameData[data.code][x].cards=hands[x]
-    gameData[data.code][x].board=board
-    var userData={
-      name:gameData[data.code][x].user,
-      money:gameData[data.code][x].money,
-      host:gameData[data.code][x].host,
-      cards:gameData[data.code][x].cards,
-      board:gameData[data.code][x].board
+    var sendData = [];
+    //console.log(gameData[data.code])
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      //console.log(gameData[data.code][x])
+      var temp = {
+        name: gameData[data.code][x].user,
+        money: gameData[data.code][x].money,
+        host: gameData[data.code][x].host,
+        cards: gameData[data.code][x].cards,
+        pot: gameData[data.code][x].pot,
+        turn: gameData[data.code][x].turn,
+        board: gameData[data.code][x].board,
+      };
+      sendData.push(temp);
     }
-    send.push(userData)
-  }
+    callback(sendData);
+  });
 
-  callback(send)
-})
+  socket.on("makeDeck", (callback) => {
+    //console.log("req",cards.draw())
 
-  socket.on("end",(data)=>{
+    callback("Deck Created");
+  });
+  socket.on("draw", (callback) => {
+    //console.log("req",cards.draw())
+
+    callback(socket.deck.draw());
+  });
+  socket.on("hands", (data, callback) => {
+    //console.log("req",cards.draw())
+    var hands = deal(gameData[data.code].length, socket.deck);
+    var board = mainBoard(socket.deck);
+    var send = [];
+
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      gameData[data.code][x].cards = hands[x];
+      gameData[data.code][x].board = board;
+      var userData = {
+        name: gameData[data.code][x].user,
+        money: gameData[data.code][x].money,
+        host: gameData[data.code][x].host,
+        cards: gameData[data.code][x].cards,
+        board: gameData[data.code][x].board,
+      };
+      send.push(userData);
+    }
+
+    callback(send);
+  });
+
+  socket.on("end", (data) => {
     var pot;
     var highest;
-    var highestUser
+    var highestUser;
     var deck;
-    for(var x = 0; x<gameData[data.code].length;x++){
-      if(gameData[data.code][x].host){
-        pot = gameData[data.code][x].pot
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      if (gameData[data.code][x].host) {
+        pot = gameData[data.code][x].pot;
         gameData[data.code][x].pot = 0;
-        deck = gameData[data.code][x].deck
+        deck = gameData[data.code][x].deck;
       }
-      if(highestUser==undefined){
-        var temp = gameData[data.code][x].board.concat(gameData[data.code][x].cards)
-        highestUser=gameData[data.code][x].user
-        highest = deck.checkMatch(temp)
-      }else{
-        var temp = gameData[data.code][x].board.concat(gameData[data.code][x].cards)
-        temp = deck.checkMatch(temp)
-        console.log(temp)
-        if(highest.rank<temp.rank){
-          highestUser=gameData[data.code][x].user
-          highest=temp;
+      if (highestUser == undefined) {
+        var temp = gameData[data.code][x].board.concat(
+          gameData[data.code][x].cards
+        );
+        highestUser = gameData[data.code][x].user;
+        highest = deck.checkMatch(temp);
+      } else {
+        var temp = gameData[data.code][x].board.concat(
+          gameData[data.code][x].cards
+        );
+        temp = deck.checkMatch(temp);
+        console.log(temp);
+        if (highest.rank < temp.rank) {
+          highestUser = gameData[data.code][x].user;
+          highest = temp;
+        } else if (highest.rank == temp.rank) {
+          if (highest.highCard > temp.highCard) {
+            highest = highest;
+          } else {
+            highest = temp;
+            highestUser = gameData[data.code][x].user;
+          }
         }
-        else if(highest.rank==temp.rank){
-         if(highest.highCard>temp.highCard){
-          highest= highest
-         }else{
-           highest=temp;
-           highestUser=gameData[data.code][x].user
-         }
-          
-        }
-      }
-
-     
-    }
-    console.log(highestUser)
-    console.log(highest)
-    for(var x = 0; x<gameData[data.code].length;x++){
-      if(highestUser==gameData[data.code][x].user){
-        gameData[data.code][x].money+=pot
-        socket.emit("newPlayer")
-        for(var x = 0; x<gameData[data.code].length;x++){
-          gameData[data.code][x].emit("newPlayer")
-        }
-        break
       }
     }
-      
-
-  })
-
-
-
+    console.log(highestUser);
+    console.log(highest);
+    for (var x = 0; x < gameData[data.code].length; x++) {
+      if (highestUser == gameData[data.code][x].user) {
+        gameData[data.code][x].money += pot;
+        socket.emit("newPlayer");
+        for (var x = 0; x < gameData[data.code].length; x++) {
+          gameData[data.code][x].emit("newPlayer");
+        }
+        break;
+      }
+    }
+  });
 });
-
-
 
 server.listen(process.env.PORT, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
-
