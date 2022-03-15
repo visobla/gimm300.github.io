12f01d30e5ad62f14a91f64c065667046ee9352d
visobla <97998552+visobla@users.noreply.github.com>
2022-03-14 18:30:06 -0600

    Violet added their games to the website

--- memory.js/memoryGame.js
+++ memory.js/memoryGame.js
@@ -1,90 +1,100 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-const cardArray=[
-
-    {
-        name: 'gold',
-        img: '../Images/violetGold.png'
-    },
-    {
-        name: 'gold',
-        img: '../Images/violetGold.png'
-    },
-    {
-        name: 'diamond',
-        img: '../Images/violetDiamond.png'
-    },
-    {
-        name: 'diamond',
-        img: '../Images/violetDiamond.png'
-    },    
-    {
-        name: 'emerald',
-        img: '../Images/violetEmerald.png'
-    },
-    {
-        name: 'emerald',
-        img: '../Images/violetEmerald.png'
-    },
-    {
+    //card options
+    const cardArray = [
+      {
         name: 'heart',
         img: '../Images/violetHeart.png'
-    },
-    {
+      },
+      {
+        name: 'gold',
+        img: '../Images/violetGold.png'
+      },
+      {
+        name: 'diamond',
+        img: '../Images/violetDiamond.png'
+      },
+      {
+        name: 'emerald',
+        img: '../Images/violetEmerald.png'
+      },
+      {
         name: 'heart',
-        img: 'Images\violetHeart.png'
-    }
-]
-cardArray.sort(() => 0.5 - Math.random())
-
-const grid = document.querySelector.apply('.grid')
-const resultDisplay = document.querySelector('result')
-var cardsChosen = []
-var cardsChosenId = []
-var cardsWon = []
-
-function createBoard(){
-    for(let i =0; i < cardArray.length; i++){
-        var card = document.createElement('img')
+        img: '../Images/violetHeart.png'
+      },
+      {
+        name: 'gold',
+        img: '../Images/violetGold.png'
+      },
+      {
+        name: 'diamond',
+        img: '../Images/violetDiamond.png'
+      },
+      {
+        name: 'emerald',
+        img: '../Images/violetEmerald.png'
+      }
+    ]
+  
+    cardArray.sort(() => 0.5 - Math.random())
+  
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
+  
+    //create your board
+    function createBoard() {
+      for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
         card.setAttribute('src', '../Images/violetBack.png')
         card.setAttribute('data-id', i)
-        card.addEventListener('click', flipcard)
+        card.addEventListener('click', flipCard)
         grid.appendChild(card)
+      }
     }
-}
-
-function checkForMatch(){
-    var cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
-    if(cardsChosen[0] === cardsChosen[1]){
-        alert('You found a match')
-        cards[optionOneId].setAttribute('src', '..Images/violetEmpty.png')
-        cards[optionTwoId].setAttribute('src', '..Images/violetEmpty.png')
-        cardsWon.push(cardsChosen)
-    } else {
+  
+    //check for matches
+    function checkForMatch() {
+      const cards = document.querySelectorAll('img')
+      const optionOneId = cardsChosenId[0]
+      const optionTwoId = cardsChosenId[1]
+      
+      if(optionOneId == optionTwoId) {
         cards[optionOneId].setAttribute('src', '../Images/violetBack.png')
         cards[optionTwoId].setAttribute('src', '../Images/violetBack.png')
-        alert('Sorry, try again')    
-    }
-    var cardsChosen = []
-    var cardsChosenId = []
-    resultDisplay.textContent = cardsWon.length
-    if (cardsWon.length === cardArray.length/2){
+        alert('You have clicked the same image!')
+      }
+      else if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', '../Images/violetEmpty.png')
+        cards[optionTwoId].setAttribute('src', '../Images/violetEmpty.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+      } else {
+        cards[optionOneId].setAttribute('src', '../Images/violetBack.png')
+        cards[optionTwoId].setAttribute('src', '../Images/violetBack.png')
+        alert('Sorry, try again')
+      }
+      cardsChosen = []
+      cardsChosenId = []
+      resultDisplay.textContent = cardsWon.length
+      if  (cardsWon.length === cardArray.length/2) {
         resultDisplay.textContent = 'Congratulations! You found them all!'
+      }
     }
-}
-
-function flipCard(){
-    var cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length ===2){
+  
+    //flip your card
+    function flipCard() {
+      let cardId = this.getAttribute('data-id')
+      cardsChosen.push(cardArray[cardId].name)
+      cardsChosenId.push(cardId)
+      this.setAttribute('src', cardArray[cardId].img)
+      if (cardsChosen.length ===2) {
         setTimeout(checkForMatch, 500)
+      }
     }
-}
-
-createBoard()
-
-})
+  
+    createBoard()
+  })
