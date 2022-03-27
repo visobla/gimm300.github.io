88384a061a4a3c798e85cd7d20530c986965d1bb
Overdrive-SRs <gentilezachary@gmail.com>
2022-03-26 19:03:41 -0600

    separated javascript and html

---
+++ Random/random.js
@@ -0,0 +1,24 @@
+function changeText() {
+		document.getElementById("test").innerHTML=("Text has changed");
+	}
+
+function changeTextBack() {
+		document.getElementById("test").innerHTML=("A paragraph");
+	}
+
+
+
+
+function changeTitleColor() {
+        document.getElementById("title").style.color = "red";
+}
+
+function changeTitleColorBack() {
+        document.getElementById("title").style.color = "black";
+}
+
+
+
+function submit() {
+        document.getElementById("submitted").innerHTML=("Submitted!");
+}
